ab2dfe37a00fbc32269ee85f00b7cc90418a826b
ro1-nc <rohan.angne@ncircletech.com>
2023-01-19 16:42:12 +0530

    adding files

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+
+namespace Multithreading_dialogbox_1_1
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        static extern int MessageBoxTimeout(IntPtr hwnd, String text, String title,
+                                     uint type, Int16 wLanguageId, Int32 milliseconds);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            MessageBoxTimeout((System.IntPtr)0, "Message", "Title", 0, 0, 15000);
+            this.Close();
+        }
+    }
+}
